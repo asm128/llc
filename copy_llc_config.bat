483faac41c5e72c2699a2e1e3eaef30deffb186d
asm128 <pabloarielz@outlook.com>
2024-12-22 18:17:43 -0300

    * Cleanup.

--- copy_llc_config.bat
+++ copy_llc_config.bat
@@ -1,9 +1,9 @@
-mkdir ..\..\Win32.Release
-mkdir ..\..\Win32.Debug
-mkdir ..\..\x64.Release
-mkdir ..\..\x64.Debug
+@REM mkdir ..\..\Win32.Release
+@REM mkdir ..\..\Win32.Debug
+@REM mkdir ..\..\x64.Release
+@REM mkdir ..\..\x64.Debug
 
-copy *.json ..\..\Win32.Release\*.*
-copy *.json ..\..\Win32.Debug\*.*
-copy *.json ..\..\x64.Release\*.*
-copy *.json ..\..\x64.Debug\*.*
+@REM copy *.json ..\..\Win32.Release\*.*
+@REM copy *.json ..\..\Win32.Debug\*.*
+@REM copy *.json ..\..\x64.Release\*.*
+@REM copy *.json ..\..\x64.Debug\*.*
